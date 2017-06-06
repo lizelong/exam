2cac27413f9c84ce806e29c97d5e78ef1589e686
unknown <474332933@qq.com>
2017-06-06 17:10:09 +0800

    增加按钮繁忙提示

--- Web/Common/Conf/config.php
+++ Web/Common/Conf/config.php
@@ -9,11 +9,11 @@ return array(
     'DB_HOST'         =>  'localhost', // 服务器地址
     'DB_NAME'         =>  'exam',          // 数据库名
     'DB_USER'         =>  'root',      // 用户名
-    'DB_PWD'          =>  '123',          // 密码
+    'DB_PWD'          =>  'Xdl2017+',          // 密码
     'DB_PORT'         =>  '3306',        // 端口
     'DB_PREFIX'       =>  'exam_',    // 数据库表前缀
 
-	'SHOW_PAGE_TRACE'	=> 	TRUE,
+	'SHOW_PAGE_TRACE'	=> 	FALSE,
 	'URL_MODEL'			=> 	2,
 
     'MODULE_ALLOW_LIST' =>    array('Home','Xdl'),//允许列表
